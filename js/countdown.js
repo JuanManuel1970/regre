fb4419d8cfeda3b373117c3f651bf2f326bb153f
Juan Manuel <juanma26@gmail.com>
2023-01-03 03:06:33 -0300

    primer comentaro2

---
+++ js/countdown.js
@@ -0,0 +1,16 @@
+simplyCountdown('#cuenta', {
+	year: 2023, // required
+	month: 1, // required
+	day: 17, // required
+	hours: 0, // Default is 0 [0-23] integer
+	minutes: 0, // Default is 0 [0-59] integer
+	seconds: 0, // Default is 0 [0-59] integer
+	words: { //words displayed into the countdown
+		days: 'Días',
+		hours: 'Hora',
+		minutes: 'Minutos',
+		seconds: 'Segundos',
+		pluralLetter: ''
+	},
+		
+})
